3aec21c3ad1b3abd4dbe6aad2433911d62844c7e
Valentin Kueck <kueck.valentin@googlemail.com>
2023-10-13 10:17:22 +0200

    remove wait screen

--- app/page.tsx
+++ app/page.tsx
@@ -20,19 +20,6 @@ export const dynamic = "force-dynamic";
 export default async function Home(searchParams: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  console.log(new Date());
-  console.log(new Date(2023, 9, 13, 10, 0, 0));
-
-  if (
-    new Date() <= new Date(2023, 9, 13, 10, 0, 0) &&
-    searchParams.searchParams.bypass != process.env.BYPASS_KEY
-  ) {
-    return (
-      <h1 className="flex w-full items-center justify-center text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
-        Die Anmeldung startet am 13.10.2023 um 10 Uhr
-      </h1>
-    );
-  }
   const slots = await db
     .select()
     .from(timeslots)
